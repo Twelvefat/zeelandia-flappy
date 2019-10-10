51d04febbf0313519186e6e61365518e3a38cde3
rfatahillah <rizalfatahillah77@gmail.com>
2019-10-10 04:29:01 -0700

    updated

---
+++ Assets/GameManage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManage : MonoBehaviour
+{
+    public GameObject gameOverCanvas,CanvasStart,ScoreCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+        Score.score = 0;
+        ScoreCanvas.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver(){
+        
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+        Score.score = addScore.current_score;
+    }
+
+    public void replay(){
+        SceneManager.LoadScene(1);
+        CanvasStart.SetActive(true);
+    }
+
+    public void tapToStart(){
+        Time.timeScale = 1;
+        CanvasStart.SetActive(false);
+        ScoreCanvas.SetActive(true);
+    }
+}
---
+++ Assets/addScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addScore : MonoBehaviour
+{
+    public static int current_score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        current_score = Score.score+=1;
+    }
+}
